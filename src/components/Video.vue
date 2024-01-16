304f26d410b04c5f7f092a95a1a093285e52b9ef
Ivan Samokhin <ivan@exton.is>
2024-01-16 20:15:27 +0000

    alpha-release

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -52,12 +52,12 @@ export default {
                 '/video/03.mp4',
                 '/video/04.mp4',
                 '/video/05.mp4',
-                '/video/06.mp4',
-                '/video/07.mp4',
-                '/video/08.mp4',
-                '/video/09.mp4',
-                '/video/10.mp4',
-                '/video/11.mp4'
+                //'/video/06.mp4',
+                //'/video/07.mp4',
+                //'/video/08.mp4',
+                //'/video/09.mp4',
+                //'/video/10.mp4',
+                //'/video/11.mp4'
             ],
             toGrey: false
         }

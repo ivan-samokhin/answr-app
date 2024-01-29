94cf4979a2eff9c6e5ecedc5601de3e6615c4368
Ivan Samokhin <ivan@exton.is>
2024-01-29 00:44:52 +0000

    release_main_1

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -218,7 +218,7 @@ export default {
     height: 25px;
   }
   .main_icons_left {
-    left: 10px;
+    left: 50px;
   }
   .main_icons_center {
     left: 50%;
@@ -226,7 +226,7 @@ export default {
   }
 
   .main_icons_right {
-    right: 30px;
+    right: 80px;
     margin-left: 0;
   }
 
